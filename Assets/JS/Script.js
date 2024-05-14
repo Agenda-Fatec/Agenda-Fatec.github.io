164d2dc29d9af4fc707b42a5ae532775a2fe455b
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2024-05-14 11:49:34 -0300

    Desenvolvendo a tela inicial e criando o menu que será usado no site.

---
+++ Assets/JS/Script.js
@@ -0,0 +1,28 @@
+function Menu_Options()
+{
+
+    document.getElementById("menu").style.transform = "translateY(0%)";
+
+}
+
+function Change_Form(formulario)
+{
+
+    switch(formulario)
+    {
+
+        case "cadastro":
+
+        
+
+        break;
+
+        default:
+
+
+
+        break;
+
+    }
+
+}
